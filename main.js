65ebd6e6f2b844a9c6f46c370537dce43a154667
moamen2020 <m01144944981@gmail.com>
2022-02-09 15:07:56 +0200

    added Sidebar navr

--- main.js
+++ main.js
@@ -6,8 +6,18 @@ dropMenu(avatar)
 dropMenu(notification)
 
 function dropMenu(selector) {
-    selector.addEventListener('click', ()=> {
-        let dropDownMenu = selector.querySelector(".dropdown-menu");
-        dropDownMenu.classList.contains("active") ? dropDownMenu.classList.remove("active") : dropDownMenu.classList.add("active");
-    })
+  selector.addEventListener('click', ()=> {
+    let dropDownMenu = selector.querySelector(".dropdown-menu");
+    dropDownMenu.classList.contains("active") ? dropDownMenu.classList.remove("active") : dropDownMenu.classList.add("active");
+  })
 }
+
+
+
+// SideBar Toggle
+let sidebar = document.querySelector('.sidebar');
+let bars = document.querySelector('.bars');
+
+bars.addEventListener('click', () => {
+  sidebar.classList.contains('active') ? sidebar.classList.remove('active') : sidebar.classList.add('active')
+})
